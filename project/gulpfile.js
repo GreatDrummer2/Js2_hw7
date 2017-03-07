f19be55e122400c702216ee91832d0bee452c1ff
GreatDrummer2 <phillippochta@yahoo.com>
2017-03-07 23:54:41 +0200

    &&&&&&&

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -1,25 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 var gulpConcat = require('gulp-concat'); 
 var jsmin = require('gulp-jsmin');
 var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 var jade = require('gulp-jade');
-
-gulp.task('scss-compile',function(){
-  return gulp.src('app/scss/**/*.scss')
-  .pipe(sass())
-  .pipe(gulp.dest('app/css'));
-});
-
-gulp.task('browser-sync',function(){
-  browserSync({
-    server:{
-      baseDir:'app'
-    }
-  })
-})
 
 gulp.task('gulp-concat',function(){
   return gulp.src(['./app/js/1.js','./app/js/2.js'])
@@ -31,13 +17,24 @@ gulp.task('minify-js', function () {
     gulp.src('app/js/all.js')
         .pipe(jsmin())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('app/'));
+        .pipe(gulp.dest('app/'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('minify-css', function() {
-  return gulp.src('app/css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('app/'));
+gulp.task('js-compile',function(){
+  return gulp.src(['./app/js/1.js','./app/js/2.js'])
+        .pipe(gulpConcat('all.js'))
+        .pipe(jsmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/'));
+})
+
+gulp.task('scss-compile',function(){
+  return gulp.src('app/scss/**/*.scss')
+  .pipe(sass())
+  //.pipe(gulp.dest('app/css'))
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(gulp.dest('app/'));
 });
 
 gulp.task('compile-jade', function() {
@@ -50,8 +47,29 @@ gulp.task('compile-jade', function() {
     .pipe(gulp.dest('./app/'))
 });
 
-gulp.task('watch',['browser-sync','minify-css','scss-compile','gulp-concat','minify-js'],function(){
-  gulp.watch('app/scss/**/*.scss',['scss-compile','minify-css'],browserSync.reload);
-  gulp.watch('app/js/**/*.js',['gulp-concat','minify-js']);
-  gulp.watch('app/**/*.html',browserSync.reload);
-})
+gulp.task('jade-watch', ['compile-jade'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('js-watch', ['js-compile'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('css-watch', ['scss-compile'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('serve', ['js-compile','scss-compile','compile-jade'], function () {
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+
+    gulp.watch('app/js/**/*.js', ['js-watch']);
+    gulp.watch('app/scss/**/*.scss', ['css-watch']);
+    gulp.watch('app/jade/**/*.jade', ['jade-watch']);
+});
